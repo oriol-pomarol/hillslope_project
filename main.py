9cf38bdd6808ce206354585871264261bf5df7f1
4946057 <o.pomarolmoya@uu.nl>
2023-12-08 17:05:22 +0100

    Independent shifts and removed zeros.

--- main.py
+++ main.py
@@ -90,8 +90,8 @@ dB_dt_steps = np.ones_like(B_input)
 dD_dt_steps = np.ones_like(D_input)
 
 # Create a jumps array to keep track of when they happen
-jumps = np.array([True] * B_input.shape[0])
-jumps[0] = False
+jumps = np.full((B_input.shape[0], B_input.shape[1]), False)
+jumps[0, :] = True
 
 # Allow the system to evolve
 for step in range(1,n_steps):
@@ -105,24 +105,17 @@ for step in range(1,n_steps):
   D_steps[step] = np.clip(D_steps[step-1] + steps_slopes[1]*dt, 0.0, alpha)
 
   # Add a random chance to set a new random B value
-  if np.random.choice([True, False], p=[prob_new_B, 1 - prob_new_B]):
-    B_steps[step] = np.random.uniform(0, c, size=len(B_steps[step]))
-    jumps[step] = False
+  jump_B = np.random.choice([True, False], size=len(B_steps[step]), p=[prob_new_B, 1 - prob_new_B])
+  B_steps[step][jump_B] = np.random.uniform(0, c, size=len(B_steps[step][jump_B]))
+  jumps[step][jump_B] = True
 
   # Add a random chance to set a new random D value
-  if np.random.choice([True, False], p=[prob_new_D, 1 - prob_new_D]):
-    D_steps[step] = np.random.uniform(0, alpha, size=len(D_steps[step]))
-    jumps[step] = False
+  jump_D = np.random.choice([True, False], size=len(D_steps[step]), p=[prob_new_D, 1 - prob_new_D])
+  D_steps[step][jump_D] = np.random.uniform(0, alpha, size=len(D_steps[step][jump_D]))
+  jumps[step][jump_D] = True
     
 dB_dt_steps[-1], dD_dt_steps[-1] = dX_dt(B_steps[-1], D_steps[-1], g[-1])
-jumps_shifted = np.roll(jumps, -1)
-
-# Save the results as the new training data
-B_input = B_steps[jumps_shifted]
-D_input = D_steps[jumps_shifted]
-g_input = g[jumps_shifted]
-dB_dt = dB_dt_steps[jumps_shifted]
-dD_dt = dD_dt_steps[jumps_shifted]
+jumps_shifted = np.roll(jumps, -1, axis=0)
 
 # Plot D(t), B(t) and g(t) for the first simulation
 fig, axs = plt.subplots(3, 1, figsize = (10,7.5))
@@ -152,6 +145,19 @@ axs[2].tick_params(axis="both", which="both", direction="in",
 fig.patch.set_alpha(1)
 plt.setp(axs, xlim=(0, n_years))
 plt.savefig(f'results/train_sim_0.png')
+
+# Mask observations where B, D, dB_dt and dD_dt are all zero
+zero_values = (B_steps == 0.0) & (D_steps == 0.0)
+
+print(f"{np.sum(zero_values)} boundary values found.")
+
+# Save the results as the new training data
+B_input = B_steps
+D_input = D_steps
+g_input = g
+dB_dt = dB_dt_steps
+dD_dt = dD_dt_steps
+
 del B_steps, D_steps, dB_dt_steps, dD_dt_steps
 
 # Save the necessary data for the system evolution
@@ -160,17 +166,12 @@ for i, element in enumerate(X_ev):
   if isinstance(element, int):
     X_ev[i] = [B_input[:,element], D_input[:,element], g[:,element]]
 
-# Define input and output variables and delete unnecessary variables
+# Define input and output variables and delete zero values
 X = np.column_stack((B_input.flatten('F'),D_input.flatten('F'),g_input.flatten('F')))
 y = np.column_stack((dB_dt.flatten('F'),dD_dt.flatten('F')))
+X = X[~(zero_values.flatten('F') | jumps_shifted.flatten('F'))]
+y = y[~(zero_values.flatten('F') | jumps_shifted.flatten('F'))]
 del B_input,D_input,g,dB_dt,dD_dt
-
-# # Remove the values where both B and D are zero 
-# zero_values = (X[:, 0] == 0.0) & (X[:, 1] == 0.0)
-
-# print(f"{np.sum(zero_values)} boundary values removed.")
-# X = X[~zero_values]
-# y = y[~zero_values]
 
 shape_data = X.shape
 print("Final data shape:", shape_data)
