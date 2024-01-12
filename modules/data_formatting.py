cd855a1c7e964ac257c3b13b79804495893f4b70
4946057 <o.pomarolmoya@uu.nl>
2024-01-12 09:41:57 +0100

    Renamed variables for consistency.

--- modules/data_formatting.py
+++ modules/data_formatting.py
@@ -3,51 +3,51 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import matplotlib.ticker as tck
 
-def data_formatting(X_jumps, y_jumps, X_lin, y_lin, sequential=False):
+def data_formatting(X_jp, y_jp, X_eq, y_eq, sequential=False):
 
-  # Set the weights for the linear and jumps data
-  w_lin = 0.5       # between 0 and 1
+  # Set the weights for the equilibrium and jumps data
+  w_eq = 0.5       # between 0 and 1
   w_train = None
 
   # Split the test data
   test_size = 0.2
 
   # Split the g_increase data between testing and the rest
-  X_lin_train, X_lin_test, y_lin_train, y_lin_test = \
-    train_test_split(X_lin, y_lin, test_size=test_size,
+  X_eq_train, X_eq_test, y_eq_train, y_eq_test = \
+    train_test_split(X_eq, y_eq, test_size=test_size,
                      shuffle=True, random_state=10)
   
   # Split the jumps data between testing and the rest and concatenate
-  n_sim_test = int(len(X_jumps)*test_size)
-  X_jumps_train = np.concatenate(X_jumps[:-n_sim_test])
-  y_jumps_train = np.concatenate(y_jumps[:-n_sim_test])
-  X_jumps_test = np.concatenate(X_jumps[-n_sim_test:])
-  y_jumps_test = np.concatenate(y_jumps[-n_sim_test:])
+  n_sim_test = int(len(X_jp)*test_size)
+  X_jp_train = np.concatenate(X_jp[:-n_sim_test])
+  y_jp_train = np.concatenate(y_jp[:-n_sim_test])
+  X_jp_test = np.concatenate(X_jp[-n_sim_test:])
+  y_jp_test = np.concatenate(y_jp[-n_sim_test:])
 
   # Split all the data between training and validation
   val_size = 0.1
-  X_lin_train, X_lin_val, y_lin_train, y_lin_val = \
-    train_test_split(X_lin_train, y_lin_train,
+  X_eq_train, X_eq_val, y_eq_train, y_eq_val = \
+    train_test_split(X_eq_train, y_eq_train,
                       test_size=val_size/(1-test_size),
                       shuffle=True, random_state=10)
-  X_jumps_train, X_jumps_val, y_jumps_train, y_jumps_val = \
-    train_test_split(X_jumps_train, y_jumps_train,
+  X_jp_train, X_jp_val, y_jp_train, y_jp_val = \
+    train_test_split(X_jp_train, y_jp_train,
                       test_size=val_size/(1-test_size),
                       shuffle=True, random_state=10)
   
-  if w_lin < 1 and w_lin > 0:
-    # Join the linear and jumps data
-    X_train = np.concatenate((X_lin_train, X_jumps_train))
-    y_train = np.concatenate((y_lin_train, y_jumps_train))
-    X_test = np.concatenate((X_lin_test, X_jumps_test))
-    y_test = np.concatenate((y_lin_test, y_jumps_test))
-    X_val = np.concatenate((X_lin_val, X_jumps_val))
-    y_val = np.concatenate((y_lin_val, y_jumps_val))
+  if w_eq < 1 and w_eq > 0:
+    # Join the equilibrium and jumps data
+    X_train = np.concatenate((X_eq_train, X_jp_train))
+    y_train = np.concatenate((y_eq_train, y_jp_train))
+    X_test = np.concatenate((X_eq_test, X_jp_test))
+    y_test = np.concatenate((y_eq_test, y_jp_test))
+    X_val = np.concatenate((X_eq_val, X_jp_val))
+    y_val = np.concatenate((y_eq_val, y_jp_val))
 
-    # Generate the weights for the linear and jumps data
-    length_ratio = len(X_lin_train)/len(X_jumps_train)
-    w_train = np.concatenate((w_lin*np.ones(len(X_lin_train))*length_ratio,
-                             (1-w_lin)*np.ones(len(X_jumps_train))))
+    # Generate the weights for the equilibrium and jumps data
+    length_ratio = len(X_eq_train)/len(X_jp_train)
+    w_train = np.concatenate((w_eq*np.ones(len(X_eq_train))*length_ratio,
+                             (1-w_eq)*np.ones(len(X_jp_train))))
     
     # Shuffle the training data
     shuffled_indices = np.arange(len(X_train))
@@ -56,35 +56,32 @@ def data_formatting(X_jumps, y_jumps, X_lin, y_lin, sequential=False):
     y_train = np.squeeze(y_train[shuffled_indices])
     w_train = np.squeeze(w_train[shuffled_indices])
 
-  elif w_lin == 0:
+  elif w_eq == 0:
     # Use only the jumps data
-    X_train = X_jumps_train
-    y_train = y_jumps_train
-    X_test = X_jumps_test
-    y_test = y_jumps_test
-    X_val = X_jumps_val
-    y_val = y_jumps_val
+    X_train = X_jp_train
+    y_train = y_jp_train
+    X_test = X_jp_test
+    y_test = y_jp_test
+    X_val = X_jp_val
+    y_val = y_jp_val
 
-  elif w_lin == 1:
-    # Use only the linear data
-    X_train = X_lin_train
-    y_train = y_lin_train
-    X_test = X_lin_test
-    y_test = y_lin_test
-    X_val = X_lin_val
-    y_val = y_lin_val
+  elif w_eq == 1:
+    # Use only the eq data
+    X_train = X_eq_train
+    y_train = y_eq_train
+    X_test = X_eq_test
+    y_test = y_eq_test
+    X_val = X_eq_val
+    y_val = y_eq_val
 
   elif sequential:
-    # Store both linear and jumps data in a list
-    X_train = [X_jumps_train, X_lin_train]
-    y_train = [y_jumps_train, y_lin_train]
-    X_test = [X_jumps_test, X_lin_test]
-    y_test = [y_jumps_test, y_lin_test]
-    X_val = [X_jumps_val, X_lin_val]
-    y_val = [y_jumps_val, y_lin_val]
-
-  else:
-    raise ValueError('Invalid mode. Valid modes are: combined, jumps, linear')
+    # Store both equilibrium and jumps data in a list
+    X_train = [X_jp_train, X_eq_train]
+    y_train = [y_jp_train, y_eq_train]
+    X_test = [X_jp_test, X_eq_test]
+    y_test = [y_jp_test, y_eq_test]
+    X_val = [X_jp_val, X_eq_val]
+    y_val = [y_jp_val, y_eq_val]
   
   # Drop a percentage of the training data for better performance
   drop_size = 0
@@ -111,7 +108,7 @@ def data_formatting(X_jumps, y_jumps, X_lin, y_lin, sequential=False):
   # Add the data characteristics to the summary
   data_summary = "".join(['\n\n***DATA FORMATTING***',
                           '\nsequential = {}'.format(sequential),
-                          '\nw_lin = {}'.format(w_lin),
+                          '\nw_eq = {}'.format(w_eq),
                           '\ntest_size = {}'.format(test_size),
                           '\nval_size = {}'.format(val_size),
                           '\ndrop_size = {}'.format(drop_size),
--- modules/data_generation.py
+++ modules/data_generation.py
@@ -18,73 +18,73 @@ def data_generation():
     # Generate the data
     # Define some run parameters for the jumps training data
     n_sim = 100                 # number of simulations to run
-    n_years = 20000             # maximum number of years to run, default 20000
+    n_years = 10000             # maximum number of years to run, default 20000
     dt = 0.5                    # time step, 7/365 in paper, default 0.5
     n_steps = int(n_years/dt)   # number of steps to run each simulation
     prob_new_state = 0.02       # probability of setting a new system state
-    prob_new_g = 0.001          # probability of setting a new random g value
+    prob_new_g = 0.002          # probability of setting a new random g value
 
     # Generate the time sequence
     t = np.linspace(0, n_years, n_steps)
 
     # Initialize B, D and g
-    B_jumps = np.zeros((n_steps, n_sim))
-    D_jumps = np.zeros((n_steps, n_sim))
-    g_jumps = np.zeros((n_steps, n_sim))
+    B_jp = np.zeros((n_steps, n_sim))
+    D_jp = np.zeros((n_steps, n_sim))
+    g_jp = np.zeros((n_steps, n_sim))
 
-    B_jumps[0] = np.random.uniform(0, c, n_sim)
-    D_jumps[0] = np.random.uniform(0, alpha, n_sim)
-    g_jumps[0] = np.random.uniform(0, 3, n_sim)
+    B_jp[0] = np.random.uniform(0, c, n_sim)
+    D_jp[0] = np.random.uniform(0, alpha, n_sim)
+    g_jp[0] = np.random.uniform(0, 3, n_sim)
 
     # Initialize dB/dt and dD/dt
-    dB_dt_jumps = np.zeros_like(B_jumps)
-    dD_dt_jumps = np.zeros_like(D_jumps)
+    dB_dt_jp = np.zeros_like(B_jp)
+    dD_dt_jp = np.zeros_like(D_jp)
 
     # Create a mask array to keep track of jumps
-    jumps_mask = np.full((n_steps, n_sim), False)
+    jp_mask = np.full((n_steps, n_sim), False)
 
     # Allow the system to evolve
     for step in range(1,n_steps):
     
         # Compute the derivatives
-        steps_slopes = dX_dt(B_jumps[step-1], D_jumps[step-1], g_jumps[step-1])
-        dB_dt_jumps[step-1], dD_dt_jumps[step-1] = steps_slopes
+        steps_slopes = dX_dt(B_jp[step-1], D_jp[step-1], g_jp[step-1])
+        dB_dt_jp[step-1], dD_dt_jp[step-1] = steps_slopes
 
         # Compute the new values, forced to be above 0 and below their theoretical max
-        B_jumps[step] = np.clip(B_jumps[step-1] + steps_slopes[0]*dt, 0.0, c)
-        D_jumps[step] = np.clip(D_jumps[step-1] + steps_slopes[1]*dt, 0.0, alpha)
-        g_jumps[step] = g_jumps[step-1]
+        B_jp[step] = np.clip(B_jp[step-1] + steps_slopes[0]*dt, 0.0, c)
+        D_jp[step] = np.clip(D_jp[step-1] + steps_slopes[1]*dt, 0.0, alpha)
+        g_jp[step] = g_jp[step-1]
 
         # Add a random chance to set a new random B value
-        jump_state = np.random.choice([True, False], size=len(B_jumps[step]), p=[prob_new_state, 1 - prob_new_state])
-        B_jumps[step][jump_state] = np.random.uniform(0, c, size=len(B_jumps[step][jump_state]))
-        D_jumps[step][jump_state] = np.random.uniform(0, alpha, size=len(D_jumps[step][jump_state]))
-        jumps_mask[step-1][jump_state] = True
+        jump_state = np.random.choice([True, False], size=len(B_jp[step]), p=[prob_new_state, 1 - prob_new_state])
+        B_jp[step][jump_state] = np.random.uniform(0, c, size=len(B_jp[step][jump_state]))
+        D_jp[step][jump_state] = np.random.uniform(0, alpha, size=len(D_jp[step][jump_state]))
+        jp_mask[step-1][jump_state] = True
 
         # Add a random chance to set a new random g value
-        jump_g = np.random.choice([True, False], size=len(g_jumps[step]), p=[prob_new_g, 1 - prob_new_g])
-        g_jumps[step][jump_g] = np.random.uniform(0, 3, size=len(g_jumps[step][jump_g]))
+        jump_g = np.random.choice([True, False], size=len(g_jp[step]), p=[prob_new_g, 1 - prob_new_g])
+        g_jp[step][jump_g] = np.random.uniform(0, 3, size=len(g_jp[step][jump_g]))
         
-    dB_dt_jumps[-1], dD_dt_jumps[-1] = dX_dt(B_jumps[-1], D_jumps[-1], g_jumps[-1])
+    dB_dt_jp[-1], dD_dt_jp[-1] = dX_dt(B_jp[-1], D_jp[-1], g_jp[-1])
 
     # Plot D(t), B(t) and g(t) for the first simulation
     fig, axs = plt.subplots(3, 1, figsize = (10,7.5))
 
-    axs[0].plot(t, D_jumps[:,0], '-b', label = 'Minimal model')
+    axs[0].plot(t, D_jp[:,0], '-b', label = 'Minimal model')
     axs[0].set_ylim(0)
     axs[0].set_ylabel('soil thickness')
     axs[0].yaxis.set_minor_locator(tck.AutoMinorLocator(2))
     axs[0].tick_params(axis="both", which="both", direction="in", 
                             top=True, right=True)
 
-    axs[1].plot(t, B_jumps[:,0], '-b')
+    axs[1].plot(t, B_jp[:,0], '-b')
     axs[1].set_ylim(0)
     axs[1].set_ylabel('biomass')
     axs[1].yaxis.set_minor_locator(tck.AutoMinorLocator(2))
     axs[1].tick_params(axis="both", which="both", direction="in", 
                             top=True, right=True)
 
-    axs[2].plot(t, g_jumps[:,0], '-b')
+    axs[2].plot(t, g_jp[:,0], '-b')
     axs[2].set_ylim(0)
     axs[2].set_ylabel('grazing pressure')
     axs[2].set_xlabel('time (years)')
@@ -94,34 +94,34 @@ def data_generation():
 
     fig.patch.set_alpha(1)
     plt.setp(axs, xlim=(0, n_years))
-    plt.savefig(f'results/train_jumps_sim_0.png')
+    plt.savefig(f'results/train_jp_sim_0.png')
 
     # Mask data where B and D are zero
-    zeros_mask = (B_jumps == 0.0) & (D_jumps == 0.0)
-    train_mask_jumps = ~(jumps_mask | zeros_mask)
+    zeros_mask = (B_jp == 0.0) & (D_jp == 0.0)
+    train_mask_jp = ~(jp_mask | zeros_mask)
 
     print(f"{np.sum(zeros_mask)} zero values found.")
 
     # Change the data into a list of arrays corresponding to each simulation
-    B_jumps = [np.squeeze(arr) for arr in np.split(B_jumps, n_sim, axis=1)]
-    D_jumps = [np.squeeze(arr) for arr in np.split(D_jumps, n_sim, axis=1)]
-    g_jumps = [np.squeeze(arr) for arr in np.split(g_jumps, n_sim, axis=1)]
-    dB_dt_jumps = [np.squeeze(arr) for arr in np.split(dB_dt_jumps, n_sim, axis=1)]
-    dD_dt_jumps = [np.squeeze(arr) for arr in np.split(dD_dt_jumps, n_sim, axis=1)]
-    train_mask_jumps = [np.squeeze(arr) for arr in np.split(train_mask_jumps, n_sim, axis=1)]
+    B_jp = [np.squeeze(arr) for arr in np.split(B_jp, n_sim, axis=1)]
+    D_jp = [np.squeeze(arr) for arr in np.split(D_jp, n_sim, axis=1)]
+    g_jp = [np.squeeze(arr) for arr in np.split(g_jp, n_sim, axis=1)]
+    dB_dt_jp = [np.squeeze(arr) for arr in np.split(dB_dt_jp, n_sim, axis=1)]
+    dD_dt_jp = [np.squeeze(arr) for arr in np.split(dD_dt_jp, n_sim, axis=1)]
+    train_mask_jp = [np.squeeze(arr) for arr in np.split(train_mask_jp, n_sim, axis=1)]
 
     # Join the data into X and y arrays
-    X_jumps = [np.column_stack((B_jumps[i], D_jumps[i], g_jumps[i])) for i in range(n_sim)]
-    y_jumps = [np.column_stack((dB_dt_jumps[i], dD_dt_jumps[i])) for i in range(n_sim)]
+    X_jp = [np.column_stack((B_jp[i], D_jp[i], g_jp[i])) for i in range(n_sim)]
+    y_jp = [np.column_stack((dB_dt_jp[i], dD_dt_jp[i])) for i in range(n_sim)]
 
     # Filter the jumps data
-    X_jumps_filtered = [np.compress(train_mask_jumps[i], X_jumps[i], axis=0) for i in range(n_sim)]
-    y_jumps_filtered = [np.compress(train_mask_jumps[i], y_jumps[i], axis=0) for i in range(n_sim)]
+    X_jp_filtered = [np.compress(train_mask_jp[i], X_jp[i], axis=0) for i in range(n_sim)]
+    y_jp_filtered = [np.compress(train_mask_jp[i], y_jp[i], axis=0) for i in range(n_sim)]
 
-    X_jumps_size = np.sum([len(X_jumps_filtered[i]) for i in range(n_sim)])
-    print(f"Final jump data size: {X_jumps_size}")
+    X_jp_size = np.sum([len(X_jp_filtered[i]) for i in range(n_sim)])
+    print(f"Final jump data size: {X_jp_size}")
     
-    # Start the system anew for the g_increase training data
+    # Start the system anew for the equilibrium training data
 
     g_init = 0.0
     B_init = c
@@ -148,47 +148,47 @@ def data_generation():
     prob_disturbance = 0.001
     strength_disturbance = 0.01
     n_steps = int(1e6)
-    g_inc = np.square(np.linspace(0, 2**0.5, n_steps))
-    B_inc = np.ones_like(g_inc) * B_init
-    D_inc = np.ones_like(g_inc) * D_init
-    dB_dt_inc = np.ones_like(g_inc) * dB_dt_init
-    dD_dt_inc = np.ones_like(g_inc) * dD_dt_init
+    g_eq = np.square(np.linspace(0, 2**0.5, n_steps))
+    B_eq = np.ones_like(g_eq) * B_init
+    D_eq = np.ones_like(g_eq) * D_init
+    dB_dt_eq = np.ones_like(g_eq) * dB_dt_init
+    dD_dt_eq = np.ones_like(g_eq) * dD_dt_init
 
     # Create a mask array to keep track of disturbances
     dist_mask = np.full(n_steps, False)
 
     # Allow the system to evolve
     for step in range(1, n_steps):
-        B_inc[step] = np.clip(B_inc[step-1] + dB_dt_inc[step-1]*dt, 0.0, c)
-        D_inc[step] = np.clip(D_inc[step-1] + dD_dt_inc[step-1]*dt, 0.0, alpha)
-        dB_dt_inc[step], dD_dt_inc[step] = dX_dt(B_inc[step], D_inc[step], g_inc[step])
+        B_eq[step] = np.clip(B_eq[step-1] + dB_dt_eq[step-1]*dt, 0.0, c)
+        D_eq[step] = np.clip(D_eq[step-1] + dD_dt_eq[step-1]*dt, 0.0, alpha)
+        dB_dt_eq[step], dD_dt_eq[step] = dX_dt(B_eq[step], D_eq[step], g_eq[step])
 
         # Add a random chance to disturb the system
         if np.random.uniform(0, 1) < prob_disturbance:
-            B_inc[step] = np.clip(B_inc[step] + np.random.uniform(-1, 1)*strength_disturbance*c, 0.0, c)
-            D_inc[step] = np.clip(D_inc[step] + np.random.uniform(-1, 1)*strength_disturbance*alpha, 0.0, alpha)
-            dB_dt_inc[step], dD_dt_inc[step] = dX_dt(B_inc[step], D_inc[step], g_inc[step])
+            B_eq[step] = np.clip(B_eq[step] + np.random.uniform(-1, 1)*strength_disturbance*c, 0.0, c)
+            D_eq[step] = np.clip(D_eq[step] + np.random.uniform(-1, 1)*strength_disturbance*alpha, 0.0, alpha)
+            dB_dt_eq[step], dD_dt_eq[step] = dX_dt(B_eq[step], D_eq[step], g_eq[step])
             dist_mask[step-1] = True
 
-    # Plot the g_increase results
-    years = np.linspace(0, n_steps*dt, len(g_inc))
+    # Plot the equilibrium data results
+    years = np.linspace(0, n_steps*dt, len(g_eq))
     fig, axs = plt.subplots(3, 1, figsize = (10,7.5))
 
-    axs[0].plot(years, D_inc, '-b', label = 'Minimal model')
+    axs[0].plot(years, D_eq, '-b', label = 'Minimal model')
     axs[0].set_ylim(0)
     axs[0].set_ylabel('soil thickness')
     axs[0].yaxis.set_minor_locator(tck.AutoMinorLocator(2))
     axs[0].tick_params(axis="both", which="both", direction="in",
                             top=True, right=True)
 
-    axs[1].plot(years, B_inc, '-b')
+    axs[1].plot(years, B_eq, '-b')
     axs[1].set_ylim(0)
     axs[1].set_ylabel('biomass')
     axs[1].yaxis.set_minor_locator(tck.AutoMinorLocator(2))
     axs[1].tick_params(axis="both", which="both", direction="in",
                             top=True, right=True)
 
-    axs[2].plot(years, g_inc, '-b')
+    axs[2].plot(years, g_eq, '-b')
     axs[2].set_ylim(0)
     axs[2].set_ylabel('grazing pressure')
     axs[2].set_xlabel('time (years)')
@@ -197,12 +197,12 @@ def data_generation():
                             top=True, right=True)
 
     fig.patch.set_alpha(1)
-    plt.savefig(f'results/train_inc.png')
+    plt.savefig(f'results/train_eq.png')
 
     # Plot the results in a B vs D plot
     fig, axs = plt.subplots(1, 1, figsize = (10,7.5))
 
-    axs.plot(D_inc, B_inc, '-ok', label='Minimal model')
+    axs.plot(D_eq, B_eq, '-ok', label='Minimal model')
     axs.set_ylim(0)
     axs.set_xlabel('soil thickness')
     axs.set_ylabel('biomass')
@@ -211,13 +211,13 @@ def data_generation():
                             top=True, right=True)
 
     fig.patch.set_alpha(1)
-    plt.savefig(f'results/train_inc_BD.png')
+    plt.savefig(f'results/train_eq_BD.png')
 
-    # Join the inc data into X and y arrays
-    X_inc = np.column_stack((B_inc[~dist_mask], D_inc[~dist_mask], g_inc[~dist_mask]))
-    y_inc = np.column_stack((dB_dt_inc[~dist_mask], dD_dt_inc[~dist_mask]))
+    # Join the equilibrium data into X and y arrays
+    X_eq = np.column_stack((B_eq[~dist_mask], D_eq[~dist_mask], g_eq[~dist_mask]))
+    y_eq = np.column_stack((dB_dt_eq[~dist_mask], dD_dt_eq[~dist_mask]))
 
-    print(f"Final g_increase data size: {len(X_inc)}")
+    print(f"Final equilibrium data size: {len(X_eq)}")
 
     # Report the data generation parameters
     gen_summary = "".join(['\n\n***DATA GENERATION:***',
@@ -227,13 +227,13 @@ def data_generation():
                            '\ndt = {}'.format(dt),
                            '\nprob_new_state = {}'.format(prob_new_state),
                            '\nprob_new_g = {}'.format(prob_new_g),
-                           '\nfinal_jumps_size = {}'.format(X_jumps_size),
+                           '\nfinal_jp_size = {}'.format(X_jp_size),
                            '\n\nEQUILIBRIUM DATA:',
                            '\nB_init = {}'.format(B_init),
                            '\nD_init = {}'.format(D_init),
                            '\nn_steps = {}'.format(n_steps),
                            '\nprob_disturbance = {}'.format(prob_disturbance),
                            '\nstrength_disturbance = {}'.format(strength_disturbance),
-                           '\nfinal_eq_size = {}'.format(len(X_inc))])
+                           '\nfinal_eq_size = {}'.format(len(X_eq))])
 
-    return gen_summary, X_jumps_filtered, y_jumps_filtered, X_inc, y_inc
+    return gen_summary, X_jp_filtered, y_jp_filtered, X_eq, y_eq
