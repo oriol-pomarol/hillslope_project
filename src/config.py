1db0ed04a47c7cf8a9e9eafb3840d894141060c7
oriol-pomarol <oriol.pomarol@gmail.com>
2024-04-08 14:49:09 +0200

    Changed config back to normal run values.

--- src/config.py
+++ src/config.py
@@ -19,18 +19,18 @@ class data_preparation:
     data_folder: str = 'detailed_larger_jumps' # Name of the folder where the data is located
     load_all: bool = False            # Load all simulations
     first_sim: int = 1               # First simulation to load (ignored if load_all is True)
-    last_sim: int = 40             # Last simulation to load (ignored if load_all is True)
+    last_sim: int = 2000             # Last simulation to load (ignored if load_all is True)
 
     # DATA GENERATION (if data_source == 'minimal')
     n_sim: int = 1000               # Number of simulations to generate
-    n_years: int = 100            # Number of years per simulation
+    n_years: int = 10000            # Number of years per simulation
     dt: float = 0.5                 # Time step in years
     prob_new_state: float = 0.02    # Probability of jumping to a new system state
     prob_new_g: float = 0.002       # Probability of jumping to a new g value
 
     # DATA FORMATTING
     mask_zeroes: bool = True        # Remove zeroes in the data (to speed up training)
-    drop_size: float = 0.999          # Percentage of data to drop (to speed up training)
+    drop_size: float = 0.0          # Percentage of data to drop (to speed up training)
     test_size: float = 0.2          # Percentage of data to use for testing
     val_size: float = 0.1           # Percentage of data to use for validation
 
@@ -45,7 +45,7 @@ class model_training:
             'learning_rate': 1E-5,
             'batch_size': 128,
             'l1_reg': 1e-5,
-            'n_epochs': 5
+            'n_epochs': 200
         }
 
     # TUNING PARAMETERS
@@ -58,7 +58,7 @@ class model_training:
 class forward_simulation:
 
     # SIMULATION PARAMETERS
-    max_years = 10   # Maximum number of years to simulate
+    max_years = 10000   # Maximum number of years to simulate
     freq_progress = 0.2 # Frequency of progress updates
 
 
